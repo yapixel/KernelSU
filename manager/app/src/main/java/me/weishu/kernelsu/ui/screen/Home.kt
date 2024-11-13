be7f15d360edd58c6ef5ac08f0ce60aefc5088d0
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-11-13 15:16:44 +0800

    manager:  failure mode dummy demo

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Home.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Home.kt
@@ -442,30 +442,6 @@ private fun StatusCard(
                 }
             }
 
-            kernelVersion.isGKI() -> {
-                Card(
-                    onClick = {
-                        if (kernelVersion.isGKI()) onClickInstall()
-                    },
-                    showIndication = true,
-                    pressFeedbackType = PressFeedbackType.Sink
-                ) {
-                    BasicComponent(
-                        title = stringResource(R.string.home_not_installed),
-                        summary = stringResource(R.string.home_click_to_install),
-                        startAction = {
-                            Icon(
-                                Icons.Rounded.ErrorOutline,
-                                stringResource(R.string.home_not_installed),
-                                modifier = Modifier
-                                    .padding(end = 16.dp),
-                                tint = colorScheme.onBackground,
-                            )
-                        }
-                    )
-                }
-            }
-
             else -> {
                 Card(
                     onClick = {
@@ -475,12 +451,12 @@ private fun StatusCard(
                     pressFeedbackType = PressFeedbackType.Sink
                 ) {
                     BasicComponent(
-                        title = stringResource(R.string.home_unsupported),
-                        summary = stringResource(R.string.home_unsupported_reason),
+                        title = stringResource(R.string.home_failure),
+                        summary = stringResource(R.string.home_failure_reason),
                         startAction = {
                             Icon(
                                 Icons.Rounded.ErrorOutline,
-                                stringResource(R.string.home_unsupported),
+                                stringResource(R.string.home_failure),
                                 modifier = Modifier
                                     .padding(end = 16.dp),
                                 tint = colorScheme.onBackground,
