688492a8f7804a67c7984e2ce1f6ff35c83d2d31
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-03 19:45:55 +0800

    kernel: expose KSU_LSM_SECURITY_HOOKS on Kconfig
    disabling this removes the need for LSM_HOOK_INIT, security_add_hooks and such,.
    Expose this and make it a configurable option.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/hook/core_hook.c
+++ kernel/hook/core_hook.c
@@ -1,11 +1,17 @@
-int ksu_inode_rename(struct inode *old_inode, struct dentry *old_dentry,
+#ifdef CONFIG_KSU_LSM_SECURITY_HOOKS
+#define LSM_HANDLER_TYPE static int
+#else
+#define LSM_HANDLER_TYPE int
+#endif
+
+LSM_HANDLER_TYPE ksu_inode_rename(struct inode *old_inode, struct dentry *old_dentry,
 			    struct inode *new_inode, struct dentry *new_dentry)
 {
 	ksu_rename_observer(old_dentry, new_dentry);
 	return 0;
 }
 
-int ksu_task_fix_setuid(struct cred *new, const struct cred *old, int flags)
+LSM_HANDLER_TYPE ksu_task_fix_setuid(struct cred *new, const struct cred *old, int flags)
 {
 	// see sys_setresuid
 	if (flags == LSM_SETID_RES)
@@ -14,12 +20,12 @@ int ksu_task_fix_setuid(struct cred *new, const struct cred *old, int flags)
 	return 0;
 }
 
-int ksu_bprm_check(struct linux_binprm *bprm)
+LSM_HANDLER_TYPE ksu_bprm_check(struct linux_binprm *bprm)
 {
 	return 0;
 }
 
-int ksu_file_permission(struct file *file, int mask)
+LSM_HANDLER_TYPE ksu_file_permission(struct file *file, int mask)
 {
 #if !defined(CONFIG_KSU_TAMPER_SYSCALL_TABLE)
 #ifdef KSU_CAN_USE_JUMP_LABEL
@@ -34,6 +40,7 @@ int ksu_file_permission(struct file *file, int mask)
 	return 0;
 }
 
+#ifdef CONFIG_KSU_LSM_SECURITY_HOOKS
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 2, 0)
 static struct security_hook_list ksu_hooks[] __ro_after_init = {
 	LSM_HOOK_INIT(inode_rename, ksu_inode_rename),
@@ -56,6 +63,21 @@ static void ksu_lsm_hook_init(void)
 }
 #endif
 #endif // 4.2
+
+#else /* ! CONFIG_KSU_LSM_SECURITY_HOOKS */
+// TEMP hooks, remove this in a month.
+int ksu_handle_setuid(struct cred *new, const struct cred *old)
+{
+	ksu_handle_setresuid_cred(new, old);
+	return 0;
+}
+int ksu_handle_rename(struct dentry *old_dentry, struct dentry *new_dentry)
+{
+	ksu_rename_observer(old_dentry, new_dentry);
+	return 0;
+}
+static inline void ksu_lsm_hook_init(void) { } // nothing, no-op
+#endif // CONFIG_KSU_LSM_SECURITY_HOOKS
 
 void __init ksu_core_init(void)
 {
