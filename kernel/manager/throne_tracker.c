023b62a8fd548ededaa87d505d5bad8a7955fec2
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-05-14 23:50:57 +0300

    kernel: throne_tracker: offload to kthread (tiann#2632)
    Run throne_tracker() in kthread instead of blocking the caller.
    Prevents full lockup during installation and removing the manager.
    
    By default, first run remains synchronous for compatibility purposes
    (FDE, FBEv1, FBEv2)
    
    Features:
    - looks and waits for manager UID in /data/system/packages.list
    - run track_throne() in a kthread after the first synchronous run
    - prevent duplicate thread creation with a single-instance check
    - spinlock-on-d_lock based polling adressing possible race conditions.
    
    Race conditions adressed
    - single instance kthread via mutex lock
    - track_throne_function, packages.list, spinlock-on-d_lock based polling
    - is_manager_apk, apk, spinlock-on-d_lock based polling
    
    This is a squash of:
    https://github.com/tiann/KernelSU/pull/2632
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>
    Co-Authored-By: backslashxx <118538522+backslashxx@users.noreply.github.com>
    Co-Authored-By: Yaroslav Zviezda <10716792+acroreiser@users.noreply.github.com>

--- kernel/manager/throne_tracker.c
+++ kernel/manager/throne_tracker.c
@@ -122,7 +122,7 @@ FILLDIR_RETURN_TYPE my_actor(struct dir_context *ctx, const char *name,
 	return FILLDIR_ACTOR_CONTINUE;
 }
 
-void search_manager(const char *path, int depth, struct list_head *uid_data)
+static noinline void search_manager(const char *path, int depth, struct list_head *uid_data)
 {
 	int i, stop = 0;
 	struct list_head data_path_list;
@@ -222,13 +222,12 @@ static bool is_uid_exist(uid_t uid, char *package, void *data)
 	return exist;
 }
 
-void track_throne(bool prune_only)
+static void throne_tracker_fn(bool prune_only)
 {
-	const struct cred *old_cred = override_creds(ksu_cred);
 	struct file *fp = filp_open(SYSTEM_PACKAGES_LIST_PATH, O_RDONLY, 0);
 	if (IS_ERR(fp)) {
 		pr_err("%s: open " SYSTEM_PACKAGES_LIST_PATH " failed: %ld\n", __func__, PTR_ERR(fp));
-		goto out_revert_cred;
+		return;
 	}
 
 	struct list_head uid_list;
@@ -316,9 +315,68 @@ out:
 	list_for_each_entry_safe (np, n, &uid_list, list) {
 		list_del(&np->list);
 		kfree(np);
-	}
-out_revert_cred:
-	revert_creds(old_cred);
+	}	
+}
+
+static DEFINE_MUTEX(throne_tracker_mutex);
+
+static int throne_tracker_thread(void *data)
+{
+	// now de-void it here
+	bool prune_only = (bool)data;
+
+	pr_info("throne_tracker: pid: %d started\n", current->pid);
+
+	guarded_mutex_lock(&throne_tracker_mutex);
+
+test_tmp:
+	if (!is_file_existing("/data/system/packages.list.tmp"))
+		goto test_list;
+
+	if (IS_ENABLED(CONFIG_KSU_DEBUG))
+		pr_info("throne_tracker: rename not finished! retry!\n");
+
+	msleep(20); // yield
+	goto test_tmp;
+
+test_list:
+	if (is_file_stable(SYSTEM_PACKAGES_LIST_PATH))
+		goto start_tt;
+
+	if (IS_ENABLED(CONFIG_KSU_DEBUG))
+		pr_info("throne_tracker: rename not finished! retry!\n");
+
+	msleep(20); // yield
+	goto test_list;	
+
+start_tt:
+	// lessen that window where user opens manager right away, yet its not crowned
+	set_user_nice(current, -10);
+
+	// this in exchange of override creds, we escape this whole thread.
+	escape_to_root_forced();
+	throne_tracker_fn(prune_only);
+
+	pr_info("throne_tracker: pid: %d exit!\n", current->pid);
+	return 0;
+}
+
+void track_throne(bool prune_only)
+{
+#ifndef CONFIG_KSU_THRONE_TRACKER_ALWAYS_THREADED
+	static void *label = &&first_run;
+	goto *label;
+
+first_run:
+	if (guarded_mutex_lock(&throne_tracker_mutex))
+		throne_tracker_fn(prune_only);
+	
+	label = &&threaded;
+	return;
+threaded:
+#endif
+	// HACK: force cast prune_only to be a void *
+	kthread_run(throne_tracker_thread, (void *)prune_only, "kthread");
 }
 
 void ksu_throne_tracker_init()
