81aadadbea5b3491801e9a18e0cb1f2bb1b82c51
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-05-18 10:38:56 +0800

    kernel: ksud: commonize execve_ksud handlers
    refactor ksu_handle_execve_ksud() and ksu_handle_compat_execve_ksud()
    to share common logic in a new helper, __ksu_handle_execve_ksud().
    
    Only the argv field is different anyway. (.native vs .compat).
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/ksud.c
+++ kernel/ksud.c
@@ -478,14 +478,13 @@ bool ksu_is_safe_mode()
 }
 
 /* 
- * ksu_handle_execve_ksud, execve_ksud handler for non kprobe
+ * ksu_handle_execve_ksud, ksu_handle_compat_execve_ksud, execve_ksud handler for non kprobe
  * adapted from sys_execve_handler_pre 
  * https://github.com/tiann/KernelSU/commit/2027ac3
  */
-__maybe_unused int ksu_handle_execve_ksud(const char __user *filename_user,
-			const char __user *const __user *__argv)
+__maybe_unused static int __ksu_handle_execve_ksud(const char __user *filename_user,
+			struct user_arg_ptr *argv)
 {
-	struct user_arg_ptr argv = { .ptr.native = __argv };
 	char path[32];
 
 	// return early if disabled.
@@ -499,7 +498,15 @@ __maybe_unused int ksu_handle_execve_ksud(const char __user *filename_user,
 	memset(path, 0, sizeof(path));
 	ksu_strncpy_from_user_nofault(path, filename_user, 32);
 
-	return __ksu_handle_execveat_ksud(AT_FDCWD, path, &argv, NULL, NULL);
+	return __ksu_handle_execveat_ksud(AT_FDCWD, path, argv, NULL, NULL);
+}
+
+// I don't think this is doable with a single entry point
+__maybe_unused int ksu_handle_execve_ksud(const char __user *filename_user,
+			const char __user *const __user *__argv)
+{
+	struct user_arg_ptr argv = { .ptr.native = __argv };
+	return __ksu_handle_execve_ksud(filename_user, &argv);
 }
 
 #if defined(CONFIG_64BIT) && defined(CONFIG_COMPAT)
@@ -507,20 +514,7 @@ __maybe_unused int ksu_handle_compat_execve_ksud(const char __user *filename_use
 			const compat_uptr_t __user *__argv)
 {
 	struct user_arg_ptr argv = { .ptr.compat = __argv };
-	char path[32];
-
-	// return early if disabled.
-	if (!ksu_execveat_hook) {
-		return 0;
-	}
-
-	if (!filename_user)
-		return 0;
-
-	memset(path, 0, sizeof(path));
-	ksu_strncpy_from_user_nofault(path, filename_user, 32);
-
-	return __ksu_handle_execveat_ksud(AT_FDCWD, path, &argv, NULL, NULL);
+	return __ksu_handle_execve_ksud(filename_user, &argv);
 }
 #endif
 
