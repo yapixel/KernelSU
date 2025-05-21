dc8bcabe13c77fe060632123dde3060ae334a9ae
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-05-21 18:59:43 +0800

    kernel: core_hook: no ext4_unregister_sysfs, no problem
    If ext4_unregister_sysfs ain't there, we don't care.
    
    This is mostly for UL builds.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -224,6 +224,7 @@ LSM_HANDLER_TYPE ksu_handle_rename(struct dentry *old_dentry, struct dentry *new
 	return 0;
 }
 
+#if defined(KSU_HAS_MODERN_EXT4) && defined(CONFIG_EXT4_FS)
 static void nuke_ext4_sysfs() {
 	struct path path;
 	int err = kern_path("/data/adb/modules", 0, &path);
@@ -243,6 +244,9 @@ static void nuke_ext4_sysfs() {
 	ext4_unregister_sysfs(sb);
  	path_put(&path);
 }
+#else
+static void nuke_ext4_sysfs() { }
+#endif
 
 LSM_HANDLER_TYPE ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
 		     unsigned long arg4, unsigned long arg5)
