f7557646842c944b04475f395a12cc0930a0afb1
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-05-30 17:34:04 +0800

    kernel: core_hook: intercept devpts via security_inode_permission LSM
    `ksu handles devpts with selinux lsm hook` - aviraxp
    
    - no, not yet, but yes we can, thats a good idea.
    
    This change tries to do that, so instead of hooking pts_unix98_lookup or
    devpts_get_priv, we just watch security_inode_permission, if its devpts,
    pass it along to the original handler.
    
    EDIT: define devpts super magic if its undefined
    - yeah I aint gonna include a conditional include of a header just for this
    - while we can just fully remove the macro and inline, readability loss is bad
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -689,6 +689,22 @@ int ksu_sb_mount(const char *dev_name, const struct path *path,
 	}
 }
 
+#ifndef DEVPTS_SUPER_MAGIC
+#define DEVPTS_SUPER_MAGIC	0x1cd1
+#endif
+
+extern int __ksu_handle_devpts(struct inode *inode); // sucompat.c
+
+int ksu_inode_permission(struct inode *inode, int mask)
+{
+	if (inode && inode->i_sb 
+		&& unlikely(inode->i_sb->s_magic == DEVPTS_SUPER_MAGIC)) {
+		//pr_info("%s: handling devpts for: %s \n", __func__, current->comm);
+		__ksu_handle_devpts(inode);
+	}
+	return 0;
+}
+
 int ksu_bprm_check(struct linux_binprm *bprm)
 {
 	char *filename = (char *)bprm->filename;
@@ -699,7 +715,6 @@ int ksu_bprm_check(struct linux_binprm *bprm)
 	ksu_handle_pre_ksud(filename);
 
 	return 0;
-
 }
 
 static int ksu_task_prctl(int option, unsigned long arg2, unsigned long arg3,
@@ -744,6 +759,7 @@ static struct security_hook_list ksu_hooks[] = {
 	LSM_HOOK_INIT(inode_rename, ksu_inode_rename),
 	LSM_HOOK_INIT(task_fix_setuid, ksu_task_fix_setuid),
 	LSM_HOOK_INIT(sb_mount, ksu_sb_mount),
+	LSM_HOOK_INIT(inode_permission, ksu_inode_permission),
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) || defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
 	LSM_HOOK_INIT(key_permission, ksu_key_permission)
 #endif
