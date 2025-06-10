3f2667ee0f142a13291934ab3d227d7f81c56afc
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-06-10 17:08:08 +0800

    kernel: ksu: printout quirks / backports / etc on init
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -2,6 +2,8 @@
 #include <linux/fs.h>
 #include <linux/kobject.h>
 #include <linux/module.h>
+#include <generated/utsrelease.h>
+#include <generated/compile.h>
 #include <linux/version.h> /* LINUX_VERSION_CODE, KERNEL_VERSION macros */
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 7, 0)
@@ -84,8 +86,42 @@ struct cred* ksu_cred;
 
 extern void ksu_supercalls_init();
 
+// track backports and other quirks here
+// ref: kernel_compat.c, Makefile
+// yes looks nasty
+#if defined(CONFIG_KSU_KPROBES_KSUD)
+	#define FEAT_1 " +kprobes_ksud"
+#else
+	#define FEAT_1 ""
+#endif
+
+#if defined(CONFIG_KSU_KRETPROBES_SUCOMPAT)
+	#define FEAT_2 " +rp_sucompat"
+#else
+	#define FEAT_2 ""
+#endif
+#if defined(CONFIG_KSU_EXTRAS)
+	#define FEAT_3 " +extras"
+#else
+	#define FEAT_3 ""
+#endif
+#if defined(CONFIG_KSU_TAMPER_SYSCALL_TABLE)
+	#define FEAT_4 " +sys_call_table_hook"
+#else
+	#define FEAT_4 ""
+#endif
+#if !defined(CONFIG_KSU_LSM_SECURITY_HOOKS)
+	#define FEAT_5 " -lsm_hooks"
+#else
+	#define FEAT_5 ""
+#endif
+
+#define EXTRA_FEATURES FEAT_1 FEAT_2 FEAT_3 FEAT_4 FEAT_5
+
 int __init kernelsu_init(void)
 {
+	pr_info("Initialized on: %s (%s) with ksuver: %s%s\n", UTS_RELEASE, UTS_MACHINE, __stringify(KSU_VERSION), EXTRA_FEATURES);
+
 #ifdef CONFIG_KSU_DEBUG
 	pr_alert("*************************************************************");
 	pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
