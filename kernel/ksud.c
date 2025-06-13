37a39a264ebf0cf1550d6110f60b71dd05bd3a3d
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-06-13 18:53:15 +0800

    kernel: ksud: handle conditional read_iter requirement for < 3.16
    nothing uses this on old kernels, so even backporting this to file_operations
    is not really needed
    though if it is found, we probably need to proxy it
    
    https://elixir.bootlin.com/linux/v3.16/source/include/linux/fs.h#L1463
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/ksud.c
+++ kernel/ksud.c
@@ -335,6 +335,7 @@ append_ksu_rc:
 	return ret;
 }
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 16, 0) || defined(KSU_HAS_FOP_READ_ITER)
 static ssize_t read_iter_proxy(struct kiocb *iocb, struct iov_iter *to)
 {
 	ssize_t ret = 0;
@@ -364,6 +365,7 @@ append_ksu_rc:
 	}
 	return ret;
 }
+#endif
 
 static int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr, size_t *count_ptr, loff_t **pos)
 {
@@ -429,12 +431,12 @@ static int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr, si
 	if (orig_read) {
 		fops_proxy.read = read_proxy;
 	}
-
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 16, 0) || defined(KSU_HAS_FOP_READ_ITER)
 	orig_read_iter = file->f_op->read_iter;
 	if (orig_read_iter) {
 		fops_proxy.read_iter = read_iter_proxy;
 	}
-
+#endif
 	// replace the file_operations
 	file->f_op = &fops_proxy;
 
