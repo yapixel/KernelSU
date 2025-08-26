a452f630a57e09e5e990abbe3e52a09663e99d91
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-08-26 23:22:25 +0800

    kernel: sucompat: provide do_execve_common handler for < 3.14
    usage on do_execve_common:
    	ksu_legacy_execve_sucompat(&filename, NULL, NULL);
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -18,16 +18,6 @@ bool ksu_queue_work(struct work_struct *work)
 	return queue_work(ksu_workqueue, work);
 }
 
-extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
-					void *argv, void *envp, int *flags);
-
-int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			void *envp, int *flags)
-{
-	return ksu_handle_execveat_sucompat(fd, filename_ptr, argv, envp,
-					    flags);
-}
-
 int __init kernelsu_init(void)
 {
 #ifdef CONFIG_KSU_DEBUG
