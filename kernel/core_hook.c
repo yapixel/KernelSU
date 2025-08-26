f7f60670efd49d002ef7243d650564fd09b5b345
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-08-26 20:36:01 +0800

    kernel: ksud: migrate ksud execution to security_bprm_check (tiann#2653)
    This migrates ksud execution decision-making to bprm_check_security.
    This requires passing proper argv and envp to a modified _ksud handler
    aptly named 'ksu_handle_bprm_ksud'.
    
    Introduces:
    int ksu_handle_bprm_ksud(const char *filename, const char *argv1,
    	const char *envp, size_t envp_len)
    
    which is adapted from:
    int ksu_handle_execveat_ksud(int *fd,
    	struct filename **filename_ptr, struct user_arg_ptr *argv,
    	struct user_arg_ptr *envp, int *flags)
    
    ksu_handle_bprm_ksud handles all the decision making, it decides when it is
    time to apply_kernelsu_rules depending if it sees "second_stage".
    
    For LSM hook, turns out we can pull out argv and envp from mm_struct.
    The code in here explains itself on how to do it.
    
    whole blob exists on arg_start to arg_end, so we just pull it out and grab next
    array after the first null terminator.
    
    as for envp, we pass the pointer then hunt for it when needed
    
    This now provides a small helper function, ksu_copy_from_user_retry, which explains
    itself. First we attempt a _nofault copy, if that fails, we try plain.
    
    With that, It also provides an inlined copy_from_user_nofault for < 5.8.
    
    While using strncpy_from_user_nofault was considered, this wont do, this will
    only copy up to the first \0.
    
    EDIT: properly move selinux init rules to this commit, its this thing that actually needs it
    - ref: https://github.com/backslashxx/KernelSU/commit/f8244dd76b561d2f387ab3197b49bb89a1e55708
    - removed on: https://github.com/tiann/KernelSU/pull/3031
    
    devlog:
    https://github.com/ximi-libra-test/android_kernel_xiaomi_libra/compare/16e5dce9e7e...16c1f5f521a
    https://github.com/backslashxx/mojito_krenol/compare/28642e60d7b...728de0c571c
    https://github.com/backslashxx/KernelSU/commit/ac730560f2e2cbf9fccc4d0f37906ba3388d344d *
    
    Stale: https://github.com/tiann/KernelSU/pull/2653
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -14,6 +14,7 @@
 #include <linux/printk.h>
 #include <linux/sched.h>
 #include <linux/stddef.h>
+#include <linux/binfmts.h>
 #include <linux/lsm_hooks.h>
 #include <linux/nsproxy.h>
 #include <linux/path.h>
@@ -231,6 +232,16 @@ int ksu_handle_setuid(struct cred *new, const struct cred *old)
 	return 0;
 }
 
+int ksu_bprm_check(struct linux_binprm *bprm)
+{
+	if (likely(!ksu_execveat_hook))
+		return 0;
+
+	ksu_handle_pre_ksud((char *)bprm->filename);
+
+	return 0;
+}
+
 static int ksu_inode_rename(struct inode *old_inode, struct dentry *old_dentry,
 			    struct inode *new_inode, struct dentry *new_dentry)
 {
@@ -247,6 +258,7 @@ static int ksu_task_fix_setuid(struct cred *new, const struct cred *old,
 static struct security_hook_list ksu_hooks[] = {
 	LSM_HOOK_INIT(inode_rename, ksu_inode_rename),
 	LSM_HOOK_INIT(task_fix_setuid, ksu_task_fix_setuid),
+	LSM_HOOK_INIT(bprm_check_security, ksu_bprm_check),
 };
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
