3cebd25e7a353887cefbb0ca0932558a90dd3faa
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-08-29 15:11:16 +0800

    manager: unofficial build
    - warn and inform that this is not an official build.
    - add emoji as promised

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Home.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Home.kt
@@ -176,6 +176,7 @@ fun HomePager(
                     if (checkUpdate) {
                         UpdateCard(themeMode)
                     }
+                    Unofficial()
                     InfoCard()
                     DonateCard()
                     LearnMoreCard()
@@ -183,6 +184,33 @@ fun HomePager(
                 Spacer(Modifier.height(bottomInnerPadding))
             }
         }
+    }
+}
+
+@Composable
+fun Unofficial() {
+    val uriHandler = LocalUriHandler.current
+    val url = stringResource(R.string.home_unofficial_kernelsu_announce)
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(),
+    ) {
+        BasicComponent(
+            title = stringResource(R.string.home_unofficial_kernelsu),
+            summary = stringResource(R.string.home_unofficial_kernelsu_body),
+            endActions = {
+                Icon(
+                    modifier = Modifier.size(28.dp),
+                    imageVector = MiuixIcons.Link,
+                    tint = colorScheme.onSurface,
+                    contentDescription = null
+                )
+            },
+            onClick = {
+                uriHandler.openUri(url)
+            }
+        )
     }
 }
 
