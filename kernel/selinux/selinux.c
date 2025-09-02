ef0e63abcc61d2352ca0c5d8af9d3148f89eaf89
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-02 17:12:39 +0800

    tweak ifdef hell

--- kernel/selinux/selinux.c
+++ kernel/selinux/selinux.c
@@ -99,63 +99,74 @@ static inline u32 current_sid(void)
 }
 #endif
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 14, 0)
 bool is_ksu_domain()
 {
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 14, 0)
 	struct lsm_context ctx;
+	bool result;
+
+	int err = security_secid_to_secctx(current_sid(), &ctx);
+	if (err)
+		return false;
+
+	result = strncmp(KERNEL_SU_DOMAIN, ctx.context, ctx.len) == 0;
+	security_release_secctx(&ctx);
+	return result;
+}
 #else
+bool is_ksu_domain()
+{
 	char *domain;
 	u32 seclen;
-#endif
 	bool result;
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 14, 0)
-	int err = security_secid_to_secctx(current_sid(), &ctx);
-#else
 	int err = security_secid_to_secctx(current_sid(), &domain, &seclen);
-#endif
 	if (err) {
 		return false;
 	}
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 14, 0)
-	result = strncmp(KERNEL_SU_DOMAIN, ctx.context, ctx.len) == 0;
-	security_release_secctx(&ctx);
-#else
 	result = strncmp(KERNEL_SU_DOMAIN, domain, seclen) == 0;
 	security_release_secctx(domain, seclen);
-#endif
 	return result;
 }
+#endif
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 14, 0)
+bool is_zygote(void *sec)
+{
+	struct task_security_struct *tsec = (struct task_security_struct *)sec;
+	if (!tsec)
+		return false;
+
+	struct lsm_context ctx;
+	bool result;
+	int err = security_secid_to_secctx(tsec->sid, &ctx);
+
+	if (err)
+		return false;
+
+	result = strncmp("u:r:zygote:s0", ctx.context, ctx.len) == 0;
+	security_release_secctx(&ctx);
+
+	return result;
+}
+#else
 bool is_zygote(void *sec)
 {
 	struct task_security_struct *tsec = (struct task_security_struct *)sec;
 	if (!tsec) {
 		return false;
 	}
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 14, 0)
-	struct lsm_context ctx;
-#else
 	char *domain;
 	u32 seclen;
-#endif
 	bool result;
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 14, 0)
-	int err = security_secid_to_secctx(tsec->sid, &ctx);
-#else
 	int err = security_secid_to_secctx(tsec->sid, &domain, &seclen);
-#endif
 	if (err) {
 		return false;
 	}
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 14, 0)
-	result = strncmp("u:r:zygote:s0", ctx.context, ctx.len) == 0;
-	security_release_secctx(&ctx);
-#else
 	result = strncmp("u:r:zygote:s0", domain, seclen) == 0;
 	security_release_secctx(domain, seclen);
-#endif
 	return result;
 }
+#endif
 
 #define DEVPTS_DOMAIN "u:object_r:ksu_file:s0"
 
