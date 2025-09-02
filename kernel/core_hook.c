e22b101ac20c26e439d5ba18bd4d87028f91b385
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-02 09:32:34 +0800

    Update core_hook.c

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -841,7 +841,7 @@ LSM_HANDLER_TYPE ksu_handle_setuid(struct cred *new, const struct cred *old)
 	// check old process's selinux context, if it is not zygote, ignore it!
 	// because some su apps may setuid to untrusted_app but they are in global mount namespace
 	// when we umount for such process, that is a disaster!
-	bool is_zygote_child = is_zygote(old->security);
+	bool is_zygote_child = ksu_is_zygote(old->security);
 	if (!is_zygote_child) {
 		pr_info("handle umount ignore non zygote child: %d\n",
 			current->pid);
