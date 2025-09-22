084dbeec8b7b506392d0fc8685116d18e8d0b07a
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-22 10:38:46 +0800

    kernel: kp_ksud: restore kprobes for early-boot and used-once hooks
    since kprobes offer dynamic hooking and shit, this is going to be better
    on something that we only need temporarily.
    
    this still keeps whole sucompat onto manual hooks as those are performance
    sensitive, needed to be permanent and "timeable".
    
    as for these hooks that got hooked here they are only used either only at
    boot or on some, used only once.
    
    old commits:
    https://github.com/backslashxx/KernelSU/commit/a4ad4c09ef2894a3e37feb700883aaad701f4117
    https://github.com/backslashxx/KernelSU/commit/60949b608bdbcf334bce08273b1c31989f18bcab
    
    symbols hooked:
    sys_reboot
    - required for ksu v2/v3. not performance sensitive so better offer it here too!
    old commit: https://github.com/backslashxx/KernelSU/commit/af4bef3619e309011cb1652cc417bba874c2d01c
    
    sys_newfstat / sys_fstat64 rp
    - upstream: https://github.com/tiann/KernelSU/commit/df640917d11dd0eff1b34ea53ec3c0dc49667002
    - seems it actually verifies the size first and only reads up to that size on next android huh
    
    --
    
    as for unregistration, we defer this once boot is completed and then have a kthread
    unregister everything.
    
    credits: some of these are just straight up copied from upstream.
    
    Tests:
    ximi mi a2 lite, arm64, Linux 4.9
    samsung galaxy s3, arm, Linux 3.0
    https://github.com/backslashxx/KernelSU/issues/26
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -67,6 +67,10 @@
 #endif
 #endif
 
+#ifdef CONFIG_KSU_KPROBES_KSUD
+#include "kp_ksud.c"
+#endif
+
 struct cred* ksu_cred;
 
 extern void ksu_supercalls_init();
@@ -106,6 +110,10 @@ int __init kernelsu_init(void)
 
 #ifdef CONFIG_KSU_TAMPER_SYSCALL_TABLE
 	ksu_syscall_table_hook_init();
+#endif
+
+#ifdef CONFIG_KSU_KPROBES_KSUD
+	kp_ksud_init();
 #endif
 
 	return 0;
