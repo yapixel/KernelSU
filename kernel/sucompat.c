6a2a937484e0cd6933c9f43836f22d5870209f21
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-25 23:42:15 +0800

    kernel: rp_sucompat: add kretprobes-hooked getname_flags for sucompat
    This introduces a kretprobe on getname_flags that improves the stealth
    and reliability of sucompat feature.
    
    Changes:
    - CONFIG_KSU_KRETPROBES_SUCOMPAT option to enable this hooking method
    - Hooks getname_flags() via kretprobe to intercept and modify filename->name
      on the return
    - prevent timing-based detections since it avoids individual syscall hijacking
      (newfstat vs newfstatat timing detections)
    - prevents doing usercopies, which in turn increases reliability on pagefaulty moments
    
    This allows sucompat to operate against anti-root detection techniques known as
    - Delayed syscall - KSU (ND)
    - sucompat SCA (Discolusre)
    - Abnormal Environment (NT)
    
    This is still very experimental, so default n, but yeah, it works.
    
    Related:
    - https://github.com/backslashxx/KernelSU/issues/5#issuecomment-2676829117
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/sucompat.c
+++ kernel/sucompat.c
@@ -277,10 +277,14 @@ int ksu_getname_flags_kernel(char **kname, int flags)
 	return ksu_sucompat_kernel_common((void *)*kname, "getname_flags", !!!flags);
 }
 
-
 #ifdef CONFIG_KSU_TAMPER_SYSCALL_TABLE
 static void syscall_table_sucompat_enable();
 static void syscall_table_sucompat_disable();
+#endif
+
+#ifdef CONFIG_KSU_KRETPROBES_SUCOMPAT
+static void rp_sucompat_exit();
+static void rp_sucompat_init();
 #endif
 
 static void ksu_sucompat_enable()
@@ -288,6 +292,10 @@ static void ksu_sucompat_enable()
 
 #ifdef CONFIG_KSU_TAMPER_SYSCALL_TABLE
 	syscall_table_sucompat_enable();
+#endif
+
+#ifdef CONFIG_KSU_KRETPROBES_SUCOMPAT
+	rp_sucompat_init();
 #endif
 
 	ksu_su_compat_enabled = true;
@@ -299,6 +307,10 @@ static void ksu_sucompat_disable()
 
 #ifdef CONFIG_KSU_TAMPER_SYSCALL_TABLE
 	syscall_table_sucompat_disable();
+#endif
+
+#ifdef CONFIG_KSU_KRETPROBES_SUCOMPAT
+	rp_sucompat_exit();
 #endif
 
 	ksu_su_compat_enabled = false;
