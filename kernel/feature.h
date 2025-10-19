b80c37674d9e7f0b67a903756619ada93400a357
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-10-19 19:49:17 +0800

    kernel: extras: SQUASH: avc log spoofing impl
    Co-Authored-By: KOWX712 <185127155+kowx712@users.noreply.github.com>

--- kernel/feature.h
+++ kernel/feature.h
@@ -7,6 +7,10 @@ enum ksu_feature_id {
 	KSU_FEATURE_SU_COMPAT = 0,
 	KSU_FEATURE_KERNEL_UMOUNT = 1,
 
+#ifdef CONFIG_KSU_EXTRAS // custom extensions
+	KSU_FEATURE_AVC_SPOOF = 10003,
+#endif
+
 	KSU_FEATURE_MAX
 };
 
