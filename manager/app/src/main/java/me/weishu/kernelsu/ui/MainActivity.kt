442fe7e000136fdd773d6441567ff0f6ee4f2bd0
KOWX712 <leecc0503@gmail.com>
2025-11-14 01:57:07 +0800

    manager: add zip intent method support to flash module
    this is a squash of:
    
    * manager: add support for opening zip file and directly flash module (KernelSU-Next/KernelSU-Next@14ec1194e401e05df607a736de36bc13b9620ce7)
    
    * manager: refine zip intent method (KOWX712/KernelSU@55e20576a9aeaca985c7b1bdfd6e2fdc429f72b7)
    
    * manager: skip installation confirmation in some condition (KOWX712/KernelSU@4e87fa6dcfa57b1464c19e3aa65a6a08f6cf54ab)
    
    Co-Authored-By: Der_Googler <54764558+dergoogler@users.noreply.github.com>
    Co-Authored-By: Rifat Azad <33044977+rifsxd@users.noreply.github.com>
    Signed-off-by: KOWX712 <leecc0503@gmail.com>

--- manager/app/src/main/java/me/weishu/kernelsu/ui/MainActivity.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package me.weishu.kernelsu.ui
 
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -29,6 +30,7 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -40,6 +42,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.animations.NavHostAnimatedDestinationStyle
+import com.ramcosta.composedestinations.generated.destinations.FlashScreenDestination
 import com.ramcosta.composedestinations.generated.NavGraphs
 import com.ramcosta.composedestinations.utils.isRouteOnBackStackAsState
 import com.ramcosta.composedestinations.utils.rememberDestinationsNavigator
@@ -49,6 +52,7 @@ import me.weishu.kernelsu.ui.theme.KernelSUTheme
 import me.weishu.kernelsu.ui.util.LocalSnackbarHost
 import me.weishu.kernelsu.ui.util.rootAvailable
 import me.weishu.kernelsu.ui.util.install
+import me.weishu.kernelsu.ui.screen.FlashIt
 
 class MainActivity : ComponentActivity() {
 
@@ -65,6 +69,18 @@ class MainActivity : ComponentActivity() {
         val isManager = Natives.isManager
         if (isManager && !Natives.requireNewKernel()) install()
 
+        // Check if launched with a ZIP file
+        val zipUri: ArrayList<Uri>? = if (intent.data != null) {
+            arrayListOf(intent.data!!)
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                intent.getParcelableArrayListExtra("uris", Uri::class.java)
+            } else {
+                @Suppress("DEPRECATION")
+                intent.getParcelableArrayListExtra("uris")
+            }
+        }
+
         setContent {
             KernelSUTheme {
                 val navController = rememberNavController()
@@ -72,6 +88,18 @@ class MainActivity : ComponentActivity() {
                 val bottomBarRoutes = remember {
                     BottomBarDestination.entries.map { it.direction.route }.toSet()
                 }
+                val navigator = navController.rememberDestinationsNavigator()
+
+                LaunchedEffect(zipUri) {
+                    if (!zipUri.isNullOrEmpty()) {
+                        navigator.navigate(
+                            FlashScreenDestination(
+                                FlashIt.FlashModules(zipUri)
+                            )
+                        )
+                    }
+                }
+
                 Scaffold(
                     bottomBar = { BottomBar(navController) },
                     contentWindowInsets = WindowInsets(0, 0, 0, 0)
