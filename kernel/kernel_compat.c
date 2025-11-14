ea396ccd40cf0232aeebaed498ee1bb474e32fa9
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-11-14 09:55:48 +0800

    kernel: core_hook: migrate init_session_keyring grab to security_bprm_check
    this supercedes:
    	kernel: fix filp_open on older kernel's kworker (#205)
    	https://github.com/tiann/KernelSU/commit/0c322a33bc22e30a9fa44a5630b9f412d601376e
    
    No need to grab this key via LSM (key_permission) / Kprobe (key_task_permission) anymore.
    ref:
    - https://github.com/tiann/KernelSU/commit/afea92b829f7a5e8621b7bc33b5390106fd1377c
    - https://github.com/tiann/KernelSU/commit/69d2a3ccba2476e500fdafef3b058929052c0181
    
    as long as it passes all those checks, the keyring we can get from
    it is surely init_session_keyring.
    
    this also makes it enabled by default on 3.8~5.2
    
    it is no longer __rcu on 5.2+ so this is just my assumption that this is
    really a < 5.2 issue. its probably just because of ACK backports that it
    works without it on 4.14 and 4.19 MSM/ACK.
    
    This is just a hypothesis as hisi and samsung kernels tend to have the issue.
    Well, they dont follow ACK 1:1 anyway.
    
    checking and adding a key is not that much of an issue, since this is mostly used
    on kthreads.
    
    As for moving this to 3.8+, it on tgcred there and with that, it seems to be not needed.
    
    This supercedes:
    - kernel: expose allowlist workaround as Kconfig option
    - https://github.com/backslashxx/KernelSU/commit/4c510ad8608cf2acffde87e3aaadca4dfd6a8b57
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/kernel_compat.c
+++ kernel/kernel_compat.c
@@ -11,7 +11,7 @@
 #include <linux/filter.h>
 #include <linux/seccomp.h>
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 8, 0) && LINUX_VERSION_CODE < KERNEL_VERSION(5, 2, 0)
 #include <linux/key.h>
 #include <linux/errno.h>
 #include <linux/cred.h>
@@ -34,20 +34,52 @@ static inline int install_session_keyring(struct key *keyring)
 
 	return commit_creds(new);
 }
-#endif
 
+// this is on tgcred on < 3.8
+// while we can grab that one, it seems to not actually be needed 
+static void ksu_grab_init_session_keyring(const char *filename)
+{
+	if (init_session_keyring)
+		return;
+		
+	if (!strstr(filename, "init")) 
+		return;
+
+	if (!!strcmp(current->comm, "init"))
+		return;
+
+	if (!!!is_init(get_current_cred()))
+		return;
+
+	// thats surely some exclamation comedy
+	// and now we are sure that this is the key we want
+	// up to 5.1, struct key __rcu *session_keyring; /* keyring inherited over fork */
+	// so we need to grab this using rcu_dereference
+	struct key *keyring = rcu_dereference(current->cred->session_keyring);
+	if (!keyring)
+		return;
+
+	init_session_keyring = key_get(keyring);
+
+	pr_info("%s: init_session_keyring: 0x%p \n", __func__, init_session_keyring);
+
+}
 struct file *ksu_filp_open_compat(const char *filename, int flags, umode_t mode)
 {
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0)
-	if (init_session_keyring != NULL && !current_cred()->session_keyring &&
-	    (current->flags & PF_WQ_WORKER)) {
-		pr_info("installing init session keyring for older kernel\n");
+	// normally we only put this on ((current->flags & PF_WQ_WORKER) || (current->flags & PF_KTHREAD))
+	// but in the grand scale of things, this does NOT matter.
+	// pr_info("installing init session keyring for older kernel\n");
+	if (init_session_keyring != NULL && !current_cred()->session_keyring) {
 		install_session_keyring(init_session_keyring);
 	}
-#endif
-	struct file *fp = filp_open(filename, flags, mode);
-	return fp;
+	return filp_open(filename, flags, mode);
 }
+#else
+struct file *ksu_filp_open_compat(const char *filename, int flags, umode_t mode)
+{
+	return filp_open(filename, flags, mode);
+}
+#endif
 
 ssize_t ksu_kernel_read_compat(struct file *p, void *buf, size_t count,
 			       loff_t *pos)
