1e662d250921e0639db84328ef11502eaf447f58
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-11-16 09:05:16 +0800

    kernel: app_profile: shim escape_with_root_profile
    this introduces kthread_escape(); which we can use to escape a kthread's  context
    to get permissions for nearly everything.
    
    fixes -13 / -EACCESS on file write/access inside ktrhead context
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/app_profile.c
+++ kernel/app_profile.c
@@ -86,7 +86,7 @@ void disable_seccomp()
 #endif
 }
 
-void escape_with_root_profile(void)
+static void escape_to_root(bool is_kthread)
 {
 	struct cred *cred;
 
@@ -96,7 +96,7 @@ void escape_with_root_profile(void)
 		return;
 	}
 
-	if (cred->euid.val == 0) {
+	if (!is_kthread && cred->euid.val == 0) {
 		pr_warn("Already root, don't escape!\n");
 		abort_creds(cred);
 		return;
@@ -145,4 +145,16 @@ void escape_with_root_profile(void)
 
 void escape_to_root_for_init(void) {
 	setup_selinux(KERNEL_SU_CONTEXT);
+}
+
+void escape_with_root_profile(void)
+{
+	escape_to_root(false);
+}
+
+void kthread_escape(void)
+{
+	// I'm not really sure which permissions are needed
+	// so lets go with this for now
+	escape_to_root(true);
 }
