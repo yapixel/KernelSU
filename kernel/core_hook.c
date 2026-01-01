f4d9bf0a207863997343d0c098e0261a7fbddf58
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-01-01 09:42:02 +0800

    kernel: ksud: migrate init.rc handling to security_file_permission LSM
    This removes the need for manually hooking sys_read / vfs_read.
    
    devlog
    https://github.com/backslashxx/KernelSU/compare/5ba658b617459b035160def783fffaf6ea127c17...8a6ae2518f190374b76fd302d19ce6a4076a2928
    https://github.com/backslashxx/KernelSU/compare/b7df5d197fea934bf7e8a4537644fe5a2705f61e...754bbd59185f0c6cede2724b46d7be7c554dafc9
    
    NOTE: https://github.com/tiann/KernelSU/commit/df640917d11dd0eff1b34ea53ec3c0dc49667002
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -239,6 +239,19 @@ int ksu_bprm_check(struct linux_binprm *bprm)
 	return 0;
 }
 
+extern bool ksu_vfs_read_hook __read_mostly;
+extern void ksu_handle_initrc(struct file *file);
+
+int ksu_file_permission(struct file *file, int mask)
+{
+	if (!ksu_vfs_read_hook)
+		return 0;
+
+	ksu_handle_initrc(file);
+
+	return 0;
+}
+
 static int ksu_inode_rename(struct inode *old_inode, struct dentry *old_dentry,
 			    struct inode *new_inode, struct dentry *new_dentry)
 {
@@ -255,6 +268,7 @@ static struct security_hook_list ksu_hooks[] = {
 	LSM_HOOK_INIT(inode_rename, ksu_inode_rename),
 	LSM_HOOK_INIT(task_fix_setuid, ksu_task_fix_setuid),
 	LSM_HOOK_INIT(bprm_check_security, ksu_bprm_check),
+	LSM_HOOK_INIT(file_permission, ksu_file_permission),
 };
 
 void __init ksu_lsm_hook_init(void)
