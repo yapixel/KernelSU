979d9e0b1abeabd28b2106172973bdc7faad943e
KOWX712 <leecc0503@gmail.com>
2026-03-12 20:22:47 +0800

    manager: QOL
    - default to material ui
    - remove warning cards
    - 32-BIT and LEGACY working mode

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/home/HomeMiuix.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/home/HomeMiuix.kt
@@ -135,9 +135,6 @@ fun HomePagerMiuix(
                         } else if (state.showKernelPrBuildWarning) {
                             WarningCard(stringResource(id = R.string.home_pr_kernel_warning), level = WarningLevel.Notice)
                         }
-                        if (state.showGkiWarning) {
-                            WarningCard(stringResource(id = R.string.home_gki_warning), level = WarningLevel.Notice)
-                        }
                         if (state.requiresNewKernel) {
                             WarningCard(
                                 stringResource(
