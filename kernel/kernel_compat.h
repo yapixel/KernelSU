57d05533ada5b3cba3c3ba5806f6f12056e62f6c
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-03-20 20:46:01 +0800

    kernel: adapt "namespace support" feature to old kernels
    TODO: add comments
    
    devlog and shit
    https://github.com/backslashxx/KernelSU/commit/6d100ac252baeb3c3bef8e94255e0d846e8d6921
    https://github.com/backslashxx/KernelSU/tree/384de6420b64850a4cf2abbb83171f151a851c7f
    https://github.com/ximi-mojito-test/mojito_krenol/commit/33ae97fb83f976ad82e73e7bbe65a421946f51d8
    https://github.com/ximi-libra-test/android_kernel_xiaomi_libra/tree/52810d22428875cca163cb52fd2819120e0b8120
    
    https://github.com/backslashxx/KernelSU/commit/7866f4e83468de715fa1a97b4e405b389b9b3c8c *
    
    upstream: https://github.com/tiann/KernelSU/pull/2909
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/kernel_compat.h
+++ kernel/kernel_compat.h
@@ -133,4 +133,32 @@ static inline void ksu_kvfree(void *buf)
 #define ksu_close_fd sys_close
 #endif
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 6, 0)
+static inline struct file *ksu_dentry_open(const struct path *path, int flags, const struct cred *cred)
+{
+	return dentry_open((*path).dentry, (*path).mnt, flags, cred);
+}
+#define dentry_open ksu_dentry_open
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 9, 0)
+__weak int path_mount(const char *dev_name, struct path *path, const char *type_page, unsigned long flags, void *data_page)
+{
+	// 384 is enough 
+	char buf[384] = {0};
+
+	// -1 on the size as implicit null termination
+	// as we zero init the thing
+	char *realpath = d_path(path, buf, sizeof(buf) - 1);
+	if (!(realpath && realpath != buf)) 
+		return -ENOENT;
+
+	mm_segment_t old_fs = get_fs();
+	set_fs(KERNEL_DS);
+	long ret = do_mount(dev_name, (const char __user *)realpath, type_page, flags, data_page);
+	set_fs(old_fs);
+	return ret;
+}
+#endif
+
 #endif // __KSU_H_KERNEL_COMPAT
