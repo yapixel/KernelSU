a618e11f030b0e4621b072fce004f6dd718dfcae
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-03-26 10:49:55 +0800

    kernel: init lsm_hook infrastructure
    we put LSM's on this.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/kernel_compat.h
+++ kernel/kernel_compat.h
@@ -1,12 +1,17 @@
 #ifndef __KSU_H_KERNEL_COMPAT
 #define __KSU_H_KERNEL_COMPAT
 
+
 #ifndef READ_ONCE
 #define READ_ONCE(x) (*(const volatile typeof(x) *)&(x))
 #endif
 
 #ifndef WRITE_ONCE
 #define WRITE_ONCE(x, y) (*(volatile typeof(x) *)&(x) = (typeof(x))(y))
+#endif
+
+#ifndef __ro_after_init
+#define __ro_after_init
 #endif
 
 #endif // __KSU_H_KERNEL_COMPAT
