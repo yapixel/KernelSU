97064005937bf6ff6eee83fa1ceac30d86b99f36
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-03-26 10:49:55 +0800

    kernel: init lsm_hook infrastructure
    we put LSM's on this.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/hook/lsm_hooks_static.c
+++ kernel/hook/lsm_hooks_static.c
@@ -11,19 +11,7 @@
  *
  */
 
-#ifndef __KSU_H_KERNEL_COMPAT
-#define __KSU_H_KERNEL_COMPAT
+static void __init ksu_core_init(void)
+{
 
-#ifndef READ_ONCE
-#define READ_ONCE(x) (*(const volatile typeof(x) *)&(x))
-#endif
-
-#ifndef WRITE_ONCE
-#define WRITE_ONCE(x, y) (*(volatile typeof(x) *)&(x) = (typeof(x))(y))
-#endif
-
-#ifndef __ro_after_init
-#define __ro_after_init
-#endif
-
-#endif // __KSU_H_KERNEL_COMPAT
+}
