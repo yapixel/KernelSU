eabf15c1ea73328d99926f335f7dbbacd9cff25e
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-03-26 13:49:07 +0800

    kernel: build: migrate to unity build
    this change moves the build system from traditionally namespaced to a unity build.
    this is on a fully single unit so that compiler can optimize hard, cut down the work,
    commonize kernel headers so much, and do some optimizations that is only possible
    or only feasible on unity build.
    
    this also upgrades mem/str functions to builtins, so people that use newer compilers
    can benefit.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

---
+++ kernel/kernel_compat.h
@@ -0,0 +1,12 @@
+#ifndef __KSU_H_KERNEL_COMPAT
+#define __KSU_H_KERNEL_COMPAT
+
+#ifndef READ_ONCE
+#define READ_ONCE(x) (*(const volatile typeof(x) *)&(x))
+#endif
+
+#ifndef WRITE_ONCE
+#define WRITE_ONCE(x, y) (*(volatile typeof(x) *)&(x) = (typeof(x))(y))
+#endif
+
+#endif // __KSU_H_KERNEL_COMPAT
