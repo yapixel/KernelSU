b7591daa52e8c01bf85d3439b478dc26fada8919
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-04-02 09:42:45 +0800

    kernel: feature: sulog: adjust for downstream
    on upstream they break this up to before and after escape_to_root
    here we just assume that its always successful, so we can skip
    request + confirmation.
    on upstream they only put this on execve's path,  so we only call
    this when caller wants to escalate.
    
    we can get argv of calling process on bprm by pulling it on current->mm
    argv is already kind of pre-flattened for us, we only need to replace null terminators
    with spaces after the first one.
    
    on upstream they pass filename + argv[x], here we pass argv0 and argv[x]
    
    EDIT: wire up jump label
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/feature/sucompat.c
+++ kernel/feature/sucompat.c
@@ -185,6 +185,9 @@ static __always_inline void ksu_sucompat_user_common(const char __user **filenam
 	if (!!__builtin_strcmp(syscall_name, "sys_execve") && !!__builtin_strcmp(syscall_name, "sys_execveat"))
 		goto no_escalate;
 
+#ifdef CONFIG_KSU_FEATURE_SULOG
+	ksu_sulog_emit(KSU_SULOG_EVENT_SUCOMPAT, NULL, NULL, GFP_KERNEL);
+#endif
 	if (!!escape_with_root_profile())
 		return;
 
@@ -278,6 +281,9 @@ static __always_inline void ksu_sucompat_kernel_common(int *restrict fd, void **
 	if (unlikely(fn_p[0] != su_p[0]))
 		return;
 
+#ifdef CONFIG_KSU_FEATURE_SULOG
+	ksu_sulog_emit(KSU_SULOG_EVENT_SUCOMPAT, NULL, NULL, GFP_KERNEL);
+#endif
 	if (!!escape_with_root_profile())
 		return;
 
