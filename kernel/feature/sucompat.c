fb4ba5b3d236667c94b45a2db016f2249d4db70f
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-04-25 20:46:57 +0800

    kernel: build: sucompat: inline fns on non-manual hook methods
    related: https://github.com/backslashxx/KernelSU/commit/7d33689f67ef447d78e54ec9b4a1ab8a6c943530
    
    cant just flatten as some kernels already have heavy inlining by def.
    lets respect kernel kbuild instead
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/feature/sucompat.c
+++ kernel/feature/sucompat.c
@@ -1,3 +1,9 @@
+#if defined(CONFIG_KSU_TAMPER_SYSCALL_TABLE) || defined(CONFIG_KSU_HACK_ARM64_BRANCH_LINK)
+#define SUCOMPAT_HOOK_TYPE static __always_inline int
+#else
+#define SUCOMPAT_HOOK_TYPE int
+#endif
+
 #define SU_PATH "/system/bin/su"
 #define SH_PATH "/system/bin/sh"
 
@@ -248,7 +254,7 @@ no_escalate:
 }
 
 // sys_faccessat
-int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode, int *__unused_flags)
+SUCOMPAT_HOOK_TYPE ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode, int *__unused_flags)
 {
 	if (!is_su_allowed((const void **)filename_user))
 		return 0;
@@ -258,7 +264,7 @@ int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
 }
 
 // sys_newfstatat, sys_fstat64
-int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
+SUCOMPAT_HOOK_TYPE ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
 {
 	if (!is_su_allowed((const void **)filename_user))
 		return 0;
@@ -268,7 +274,7 @@ int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
 }
 
 // sys_execve, compat_sys_execve
-int ksu_handle_sys_execve(const char __user **filename_user, void *argv, void *envp)
+SUCOMPAT_HOOK_TYPE ksu_handle_sys_execve(const char __user **filename_user, void *argv, void *envp)
 {
 #ifdef CONFIG_KSU_FEATURE_ADBROOT
 	ksu_adb_root_execve_user((void *)filename_user, (void *)envp);
@@ -281,7 +287,7 @@ int ksu_handle_sys_execve(const char __user **filename_user, void *argv, void *e
 }
 
 // sys_execveat, compat_sys_execveat
-int ksu_handle_sys_execveat(int *fd, const char __user **filename_user, void *argv, void *envp, int *flags)
+SUCOMPAT_HOOK_TYPE ksu_handle_sys_execveat(int *fd, const char __user **filename_user, void *argv, void *envp, int *flags)
 {
 #ifdef CONFIG_KSU_FEATURE_ADBROOT
 	ksu_adb_root_execve_user((void *)filename_user, (void *)envp);
@@ -357,7 +363,7 @@ no_ksud:
 }
 
 struct filename; // take note: struct filename *filename, for do_execveat_common / do_execve_common on >= 3.14
-int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags)
+SUCOMPAT_HOOK_TYPE ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags)
 {
 	void *struct_filename = *(void **)filename_ptr;
 	if (IS_ERR(struct_filename)) // see getname_flags
@@ -371,7 +377,7 @@ int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv, voi
 }
 
 // take note: char *filename, for do_execve_common on < 3.14
-int ksu_legacy_execve_sucompat(const char **filename_ptr, void *argv, void *envp)
+SUCOMPAT_HOOK_TYPE ksu_legacy_execve_sucompat(const char **filename_ptr, void *argv, void *envp)
 {
 	ksu_sucompat_kernel_common((int *)AT_FDCWD, (void **)filename_ptr, argv, envp, 0, "do_execve_common");
 	return 0;
