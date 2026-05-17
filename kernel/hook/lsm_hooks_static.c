2aae49b82f585cd87b6ee976d208287a59a450e7
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-05-17 14:17:27 +0800

    kernel: lsm_hook: wire up setprocattr lsm hack for selinux_hide
    devlog:
    https://github.com/backslashxx/KernelSU/commit/e181dd7ec583d162666636d9dc9e836ce1732cb2
    https://github.com/backslashxx/KernelSU/commit/444264ff7cc651c5ad398a98d02da6da12dae985
    https://github.com/backslashxx/KernelSU/commit/868ce4b55a50ed62ae0e47a03a4e59a5558958b9
    
    TODO: proper commetn and notes
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/hook/lsm_hooks_static.c
+++ kernel/hook/lsm_hooks_static.c
@@ -78,6 +78,13 @@ static int ksu_security_file_permission(struct file *file, int mask)
 	return security_file_permission(file, mask);
 }
 
+extern int security_setprocattr(int lsmid, const char *name, void *value, size_t size);
+static int ksu_setprocattr(int lsmid, const char *name, void *value, size_t size)
+{
+	ksu_hide_setprocattr_inline(name, value, size);
+	return security_setprocattr(lsmid, name, value, size);
+}
+
 static void __init ksu_core_init(void)
 {
 	int ret;
@@ -166,5 +173,10 @@ read_hook_done:
 	pr_info("lsm_hijack: security_file_permission: ret %d \n", ret);
 	;
 #endif
+
+	target_callsite = kallsyms_lookup_retry("proc_pid_attr_write");
+	symbol_addr = kallsyms_lookup_retry("security_setprocattr");
+	ret = arm64_bl_patch(target_callsite, ksu_get_ksym_size(target_callsite, 128 * sizeof(uint32_t)), symbol_addr, (uintptr_t)&ksu_setprocattr);
+	pr_info("lsm_hijack: security_setprocattr: ret %d \n", ret);
 
 }
