8ccfd539ee1494080f6083e0830a502e5f608c2b
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-05-17 14:17:27 +0800

    kernel: lsm_hook: wire up setprocattr lsm hack for selinux_hide
    devlog:
    https://github.com/backslashxx/KernelSU/commit/e181dd7ec583d162666636d9dc9e836ce1732cb2
    https://github.com/backslashxx/KernelSU/commit/444264ff7cc651c5ad398a98d02da6da12dae985
    https://github.com/backslashxx/KernelSU/commit/868ce4b55a50ed62ae0e47a03a4e59a5558958b9
    
    TODO: proper commetn and notes
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/hook/lsm_hooks_list.c
+++ kernel/hook/lsm_hooks_list.c
@@ -72,6 +72,48 @@ capability_fn:
 }
 #endif
 
+static void *setprocattr_fn __read_mostly = nullptr;
+static __nocfi int ksu_setprocattr_new(const char *name, void *value, size_t size)
+{
+	assume(!!setprocattr_fn);
+	ksu_hide_setprocattr_inline(name, value, size);
+	return ((typeof(ksu_setprocattr_new) *)setprocattr_fn)(name, value, size);
+}
+
+static __nocfi int ksu_setprocattr_old(struct task_struct *p, char *name, void *value, size_t size)
+{
+	assume(!!setprocattr_fn);
+
+	ksu_hide_setprocattr_inline(name, value, size);
+	return ((typeof(ksu_setprocattr_old) *)setprocattr_fn)(p, name, value, size);
+}
+
+#define SETPROCATTR_TYPE_old	struct task_struct *, char *, void *, size_t
+#define SETPROCATTR_TYPE_new1	const char *, void *, size_t
+#define SETPROCATTR_TYPE_new2	const char *lsm, const char *, void *, size_t
+
+/**
+ * workaround for GCC 4.9's broken designated initializer.
+ * - avoid initializing it casted.
+ * e.g. (void *)ksu_setprocattr_old, (void *)ksu_setprocattr_new
+ */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wincompatible-pointer-types"
+#define OVERLOAD_SETPROCATTR(fn) _Generic(			\
+(fn),								\
+	int (*)(SETPROCATTR_TYPE_old)	:ksu_setprocattr_old,	\
+	int (*)(SETPROCATTR_TYPE_new1)	:ksu_setprocattr_new, 	\
+	int (*)(SETPROCATTR_TYPE_new2)	:ksu_setprocattr_new 	\
+)
+
+// now choose what we have
+static typeof(security_setprocattr) *ksu_setprocattr __read_mostly = OVERLOAD_SETPROCATTR(security_setprocattr);
+#pragma GCC diagnostic pop
+#undef SETPROCATTR_TYPE_new2
+#undef SETPROCATTR_TYPE_new1
+#undef SETPROCATTR_TYPE_old
+#undef OVERLOAD_SETPROCATTR
+
 /**
  *
  * Instead of using list/hlist abstractions and shit, since we know these things exist
@@ -193,6 +235,7 @@ static __init void ksu_lsm_hook_init(void)
 {
 	LSM_HACK_INIT(task_fix_setuid, ksu_task_fix_setuid);
 	LSM_HACK_INIT(inode_rename, ksu_inode_rename);
+	LSM_HACK_INIT(setprocattr, ksu_setprocattr);
 
 #ifdef CONFIG_KSU_FEATURE_SULOG
 	LSM_HACK_INIT(bprm_committing_creds, ksu_bprm_committing_creds);
