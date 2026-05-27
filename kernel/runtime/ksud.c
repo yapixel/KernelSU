7227513347a076dea43535040618888c046079c2
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-05-27 23:16:22 +0800

    kernel: ksud: handle early newfstat module_rc size injection
    it can happen that this is too early that we dont have working cred yet
    so we grab length via vfs instead
    
    [    2.187511] KernelSU: module rc: open /metadata/ksu/modules.rc failed: -13
    
    upstream
    https://github.com/tiann/KernelSU/pull/3478
    https://github.com/tiann/KernelSU/commit/9ab31068345d5811160d74ff0dec79c51b1ce9c3
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/runtime/ksud.c
+++ kernel/runtime/ksud.c
@@ -91,7 +91,7 @@ const size_t ksu_rc_len = sizeof(KERNEL_SU_RC) - 1;
 #define MODULE_RC_PATH_DEFAULT "/metadata/ksu/modules.rc"
 #define MODULE_RC_MAX (1u << 20) /* 1 MiB cap */
 static char *module_rc_buf;
-static size_t module_rc_len;
+static size_t module_rc_len = 0;
 static ssize_t module_rc_pos;
 
 static struct file *open_module_rc(const char **chosen_path)
@@ -173,6 +173,36 @@ static void free_module_rc(void)
 	kvfree(module_rc_buf);
 	module_rc_buf = NULL;
 	module_rc_len = 0;
+}
+
+static noinline void set_module_rc_len_vfs()
+{
+	static bool loaded = false;
+	if (loaded)
+		return;
+
+	loaded = true;
+
+	struct path path;
+
+	int err = kern_path(MODULE_RC_PATH_WATCHDOG, LOOKUP_FOLLOW, &path);
+	if (err)
+		err = kern_path(MODULE_RC_PATH_DEFAULT, LOOKUP_FOLLOW, &path);
+	if (err)
+		return; 
+
+	struct inode *inode = d_inode(path.dentry);
+	if (inode && S_ISREG(inode->i_mode))
+		module_rc_len = i_size_read(inode);
+
+	path_put(&path);
+
+	if (module_rc_len > MODULE_RC_MAX)
+		module_rc_len = MODULE_RC_MAX;
+
+	pr_info("%s: %zu\n", __func__, module_rc_len);
+
+	return;
 }
 
 // https://cs.android.com/android/platform/superproject/main/+/main:system/core/init/parser.cpp;l=144;drc=61197364367c9e404c7da6900658f1b16c42d0da
@@ -395,6 +425,12 @@ static inline void ksu_common_newfstat_ret(unsigned int fd_int, void **statbuf_p
 	void __user *statbuf = (void __user *)statbuf_ptr_local;
 	if (!statbuf)
 		return;
+
+	// if init process is running, try to grab module_rc length
+	// preempt check is because we are also running newfstat hook on kprobe
+	// and we really cannot kern_path on it safely
+	if (preemptible())
+		set_module_rc_len_vfs();
 
 	pr_info("%s: stat init.rc \n", syscall_name);
 
