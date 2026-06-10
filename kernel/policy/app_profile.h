27fcb943ba06674dddbc2208a7be0f2ed36f988b
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-06-11 07:28:23 +0800

    kernel: app_profile: add 32-bit KSU_NO_NEW_PRIVS tif support
    upstream: https://github.com/tiann/KernelSU/pull/3509
    
    its free real estate, this is ulong so wordsize.
    https://elixir.bootlin.com/linux/v7.0-rc6/source/arch/arm64/include/asm/thread_info.h
    https://elixir.bootlin.com/linux/v7.0-rc6/source/arch/arm/include/asm/thread_info.h
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/policy/app_profile.h
+++ kernel/policy/app_profile.h
@@ -1,7 +1,17 @@
 #ifndef __KSU_H_APP_PROFILE
 #define __KSU_H_APP_PROFILE
 
+#if defined(CONFIG_64BIT)
 #define TIF_KSU_DISABLE_ESCAPE_WITH_ROOT 63
+#define TIF_KSU_RESERVED_62 62
+#define TIF_KSU_UNMOUNTABLE 61
+#define TIF_KSU_MANAGED	60
+#else
+#define TIF_KSU_DISABLE_ESCAPE_WITH_ROOT 31
+#define TIF_KSU_RESERVED_30 30
+#define TIF_KSU_UNMOUNTABLE 29
+#define TIF_KSU_MANAGED	28
+#endif
 
 // Escalate current process to root with the appropriate profile
 int escape_with_root_profile(void);
