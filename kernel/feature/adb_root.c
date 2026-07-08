a5a843e8f79188b558afb620a15f4957f1e8c2aa
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-07-08 12:35:55 +0800

    Update adb_root.c

--- kernel/feature/adb_root.c
+++ kernel/feature/adb_root.c
@@ -21,6 +21,99 @@ static long is_libadbroot_ok()
 }
 
 // TODO: implement downstream
+static long setup_ld_preload(struct pt_regs *regs)
+{
+	static const char kLdPreload[] = "LD_PRELOAD=/data/adb/ksu/lib/libadbroot.so";
+	static const char kLdLibraryPath[] = "LD_LIBRARY_PATH=/data/adb/ksu/lib";
+	static const size_t kReadEnvBatch = 16;
+	static const size_t kPtrSize = sizeof(unsigned long);
+	unsigned long stackp = user_stack_pointer(regs);
+	unsigned long envp, ld_preload_p, ld_library_path_p;
+	unsigned long *envp_p = (unsigned long *)&PT_REGS_PARM3(regs);
+	unsigned long *tmp_env_p = NULL, *tmp_env_p2 = NULL;
+	size_t env_count = 0, total_size;
+	long ret;
+
+	envp = (char __user **)untagged_addr((unsigned long)*envp_p);
+
+	ld_preload_p = stackp = ALIGN_DOWN(stackp - sizeof(kLdPreload), 8);
+	ret = copy_to_user(ld_preload_p, kLdPreload, sizeof(kLdPreload));
+	if (ret != 0) {
+		pr_warn("write ld_preload when adb_root_handle_execve failed: %ld\n", ret);
+		return -EFAULT;
+	}
+
+	ld_library_path_p = stackp = ALIGN_DOWN(stackp - sizeof(kLdLibraryPath), 8);
+	ret = copy_to_user(ld_library_path_p, kLdLibraryPath, sizeof(kLdLibraryPath));
+	if (ret != 0) {
+		pr_warn("write ld_library_path when adb_root_handle_execve failed: %ld\n", ret);
+		return -EFAULT;
+	}
+
+	for (;;) {
+		tmp_env_p2 = krealloc(tmp_env_p, (env_count + kReadEnvBatch + 2) * kPtrSize, GFP_KERNEL);
+		if (tmp_env_p2 == NULL) {
+			pr_err("alloc tmp env failed\n");
+			ret = -ENOMEM;
+			goto out_release_env_p;
+		}
+		tmp_env_p = tmp_env_p2;
+		ret = copy_from_user(&tmp_env_p[env_count], envp + env_count * kPtrSize, kReadEnvBatch * kPtrSize);
+		if (ret < 0) {
+			pr_warn("Access envp when adb_root_handle_execve failed: %ld\n", ret);
+			ret = -EFAULT;
+			goto out_release_env_p;
+		}
+		size_t read_count = kReadEnvBatch * kPtrSize - ret;
+		size_t max_new_env_count = read_count / kPtrSize, new_env_count = 0;
+		bool meet_zero = false;
+		for (; new_env_count < max_new_env_count; new_env_count++) {
+			if (!tmp_env_p[new_env_count + env_count]) {
+				meet_zero = true;
+				break;
+			}
+		}
+		if (!meet_zero) {
+			if (read_count % kPtrSize != 0) {
+				pr_err("unaligned envp array!\n");
+				ret = -EFAULT;
+				goto out_release_env_p;
+			} else if (ret != 0) {
+				pr_err("truncated envp array!\n");
+				ret = -EFAULT;
+				goto out_release_env_p;
+			}
+		}
+		env_count += new_env_count;
+		if (meet_zero)
+			break;
+	}
+
+	// We should have allocated enough memory
+	// TODO: handle existing LD_PRELOAD
+	tmp_env_p[env_count++] = ld_preload_p;
+	tmp_env_p[env_count++] = ld_library_path_p;
+	tmp_env_p[env_count++] = 0;
+	total_size = env_count * kPtrSize;
+
+	stackp -= total_size;
+	ret = copy_to_user(stackp, tmp_env_p, total_size);
+	if (ret != 0) {
+		pr_err("copy new env failed: %ld\n", ret);
+		ret = -EFAULT;
+		goto out_release_env_p;
+	}
+
+	*envp_p = stackp;
+	ret = 0;
+
+out_release_env_p:
+	if (tmp_env_p) {
+		kfree(tmp_env_p);
+	}
+
+	return ret;
+}
 
 static int kernel_adb_root_feature_get(u64 *value)
 {
