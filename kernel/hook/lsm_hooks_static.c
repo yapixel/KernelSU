2048875cf920bf7c21d481065ee99089bdb57509
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-08-04 18:18:13 +0800

    kernel: init lsm_hook infrastructure
    we put LSM's on this.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

---
+++ kernel/hook/lsm_hooks_static.c
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2026 \xx
+ *
+ * This file is a downstream extension and NOT affiliated, endorsed by,
+ * or maintained by the official KernelSU developers.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
+static void __init ksu_core_init(void)
+{
+
+}
