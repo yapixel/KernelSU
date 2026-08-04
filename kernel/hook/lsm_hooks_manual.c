4d4bd56d83c77b5192f8bf74224231fede56544a
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-08-04 18:18:13 +0800

    kernel: init lsm_hook infrastructure
    we put LSM's on this.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

---
+++ kernel/hook/lsm_hooks_manual.c
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2026 \xx
+ *
+ * This file is a downstream extension and NOT affiliated, endorsed by,
+ * or maintained by the official KernelSU developers.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
+int ksu_inode_rename(struct inode *old_inode, struct dentry *old_dentry,
+			    struct inode *new_inode, struct dentry *new_dentry)
+{
+	return 0;
+}
+
+int ksu_task_fix_setuid(struct cred *new, const struct cred *old, int flags)
+{
+	return 0;
+}
+
+int ksu_bprm_check(struct linux_binprm *bprm)
+{
+	return 0;
+}
+
+static void __init ksu_core_init(void)
+{
+	pr_info("%s: Automated LSM hooking disabled. Make sure manual security hooks are implemented!\n", __func__);
+}
